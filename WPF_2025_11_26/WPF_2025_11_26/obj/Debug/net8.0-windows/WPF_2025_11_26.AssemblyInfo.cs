f4d7ef20488025014b12a80de31e09534647c33e
ilyescsaba-prog <ilyescsaba@taszi.hu>
2025-11-26 14:25:53 +0100

    tokmindegy

--- WPF_2025_11_26/WPF_2025_11_26/obj/Debug/net8.0-windows/WPF_2025_11_26.AssemblyInfo.cs
+++ WPF_2025_11_26/WPF_2025_11_26/obj/Debug/net8.0-windows/WPF_2025_11_26.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF_2025_11_26")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd167aaa2b2396acbc5c01b42dcdbceff12ebe57")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF_2025_11_26")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF_2025_11_26")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
